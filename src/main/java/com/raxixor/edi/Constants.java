9431e1d438c1e13720bb4f093d247c4102eb1def
Rax Ixor <raxixor@gmail.com>
2017-03-15 23:04:12 +0200

    New Constants & TextChannelCommand finished.

--- src/main/java/com/raxixor/edi/Constants.java
+++ src/main/java/com/raxixor/edi/Constants.java
@@ -11,7 +11,9 @@ public class Constants {
     public final static String WARNING = "\u2622";
     public final static String ERROR = "\u203c";
     public final static String NEED_MENTION = ERROR + " | Please mention at least 1 %s!";
+    public final static String NEED_ONE_MENTION = ERROR + " | Please mention 1 %s!";
     public final static String NEED_X = ERROR + " | Please include at least 1 %s!";
+    public final static String NEED_ONE = ERROR + " | Please include 1 %s!";
     public final static String SERVER_INVITE = "https://discord.gg/PDBCygF";
     public final static String BOT_INVITE = "https://discordapp.com/oauth2/authorize?client_id=248729096106803200&scope=bot&permissions=2080898175";
     public final static String OWNER_ID = "145399954574147584";
--- src/main/java/com/raxixor/edi/commands/channel/TextChannelCommand.java
+++ src/main/java/com/raxixor/edi/commands/channel/TextChannelCommand.java
@@ -1,8 +1,11 @@
 package com.raxixor.edi.commands.channel;
 
 import com.raxixor.edi.Bot;
+import com.raxixor.edi.Constants;
+import com.sun.istack.internal.Nullable;
 import me.jagrosh.jdautilities.commandclient.Command;
 import me.jagrosh.jdautilities.commandclient.CommandEvent;
+import net.dv8tion.jda.core.entities.TextChannel;
 
 /**
  * Created by raxix on 15/03/2017, 22:38.
@@ -15,12 +18,38 @@ public class TextChannelCommand extends Command {
 		this.guildOnly = true;
 		this.ownerCommand = false;
 		this.category = bot.MOD;
-		this.arguments = "#channel | new | edit";
+		this.arguments = "new <name>";
 	}
 	
 	@Override
 	protected void execute(CommandEvent event) {
+		if (!(event.getArgs().toLowerCase().startsWith("new"))) {
+			event.reply(event.getClient().getError() + " | No valid arguments provided!");
+			return;
+		}
+		if (event.getArgs().toLowerCase().startsWith("new") && event.getArgs().length() < 4) {
+			event.reply(event.getClient().getError() + " | Missing name for new channel.");
+			return;
+		}
 		
+		if (event.getArgs().toLowerCase().startsWith("new")) {
+			executeNew(event.getArgs().substring(4), event, null);
+		}
 	}
 	
+	/**
+	 * Executes the creation of a new channel.
+	 * 
+	 * @param name Name for the new channel.
+	 * @param event The CommandEvent from the command execution.
+	 * @param extra Extra information.
+	 */
+	protected void executeNew(String name, CommandEvent event, @Nullable String extra) {
+		event.getGuild().getController().createTextChannel(name).queue((v) -> {
+			event.reply(event.getClient().getSuccess() + " | Successfully created text channel " + v.getAsMention() + "!");
+		}, (t) -> {
+			event.reply(event.getClient().getError() + " | I failed to create a text channel named `" + name + "`!\n"
+			+ t.getMessage());
+		});
+	}
 }

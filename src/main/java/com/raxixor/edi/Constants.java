16de052056c2f4cbb35b2ce0292219b659a5d10f
Rax Ixor <raxixor@gmail.com>
2017-03-16 12:02:16 +0200

    Changed constants slightly. Updated gitignore.

--- src/main/java/com/raxixor/edi/Constants.java
+++ src/main/java/com/raxixor/edi/Constants.java
@@ -14,7 +14,7 @@ public class Constants {
     public final static String NEED_ONE_MENTION = ERROR + " | Please mention 1 %s!";
     public final static String NEED_X = ERROR + " | Please include at least 1 %s!";
     public final static String NEED_ONE = ERROR + " | Please include 1 %s!";
-    public final static String SERVER_INVITE = "https://discord.gg/PDBCygF";
+    public final static String SUPPORT_SERVER_INVITE = "https://discord.gg/34JHEru";
     public final static String BOT_INVITE = "https://discordapp.com/oauth2/authorize?client_id=248729096106803200&scope=bot&permissions=2080898175";
     public final static String OWNER_ID = "145399954574147584";
 }
